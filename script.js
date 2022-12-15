b8a930e30a83d247703e03f718617c1abd66e615
tmschutter <tmschutter@gmail.com>
2022-12-15 15:03:57 -0700

    It's fun now :)

--- script.js
+++ script.js
@@ -3,24 +3,30 @@ const body = document.getElementsByTagName('body')[0]
 function makeSquare(color){
     var div = document.createElement('div')
     div.className = `${color} square`
-    div.style.width = '11.1%'
-    div.style.height = '11.1%'
+    div.style.width = '1%'
+    div.style.height = '1%'
     div.style.float = 'left'
-    div.style.paddingBottom = '11.1%'
+    div.style.paddingBottom = '1%'
     div.style.backgroundColor = color
-    body.append(div)
+    div.addEventListener('mouseover', randColorEvt)
+    return div
 }
 
 function checkers(num){
     let i = 0
     let blk = true
     while (i < num){
+        // let randColor = '#' + Math.floor(Math.random()*16777215).toString(16);
         if (blk){
-            makeSquare('black')
-        } else makeSquare('white')
+            body.append(makeSquare('white'))
+        } else body.append(makeSquare('white'))
         i++
         blk = !blk
     }
 }
 
-checkers(81)
+checkers(5000)
+
+function randColorEvt(e){
+    e.target.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+}
