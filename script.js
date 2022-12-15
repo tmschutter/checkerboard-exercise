775873f540b216f8e3019adf3f538f0d3026666b
tmschutter <tmschutter@gmail.com>
2022-12-14 18:17:51 -0700

    ver 0.1

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// Your JS goes here
+const body = document.getElementsByTagName('body')[0]
+
+function makeSquare(color){
+    var div = document.createElement('div')
+    div.className = `${color} square`
+    div.style.width = '11.1%'
+    div.style.height = '11.1%'
+    div.style.float = 'left'
+    div.style.paddingBottom = '11.1%'
+    div.style.backgroundColor = color
+    body.append(div)
+}
+
+function checkers(num){
+    let i = 0
+    let blk = true
+    while (i < num){
+        if (blk){
+            makeSquare('black')
+        } else makeSquare('white')
+        i++
+        blk = !blk
+    }
+}
+
+checkers(81)
